ecc39f4a3d3c32c6ba2b8e32bacb50f94e555e09
DonovanPutra <donovanputra25@gmail.com>
2024-02-20 12:48:13 +0700

    menambahkan fungsi perkalian

--- main.py
+++ main.py
@@ -3,8 +3,12 @@
 def addition(a, b):
 	result = a + b
 	return result
+def multiplication(c, d):
+	result =  c * d
+	return result
 def main():
 	print(addition(20,20))
+	print(multiplication(10, 10))
 
 if __name__ == "__main__":
 	main()
